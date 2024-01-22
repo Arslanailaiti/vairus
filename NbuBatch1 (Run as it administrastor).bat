9024ef67a4486de4b826867700a0fea09abb9032
kyrok638 <134541021+Arslanailaiti@users.noreply.github.com>
2024-01-22 09:22:38 -0800

    Update NbuBatch1 (Run as it administrastor).bat

--- NbuBatch1 (Run as it administrastor).bat
+++ NbuBatch1 (Run as it administrastor).bat
@@ -1,6 +1,6 @@
 @echo off
 title NBUBATCH7928192810221382389139128391283hasjfnwheiurhwr43u28904eu2308eujqwouqwioueiqh49234
-RUN? THIS IS DANGER
+echo RUN? THIS IS DANGER
 pause
 cls
 echo msgbox "You are trashed by NbuBatch1.0.bat" >x.vbs
